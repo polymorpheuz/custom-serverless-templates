8caf5ba59eb156019c1daeabcabd5d5092627df0
polymorpheuz <polymorpheuz@gmail.com>
2020-02-06 11:27:26 +0200

    feat: added minimalistic node template

---
+++ templates/node-min-init/index.js
@@ -0,0 +1,14 @@
+module.exports.main = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+
